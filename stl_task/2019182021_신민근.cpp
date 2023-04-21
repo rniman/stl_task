c28861ecc4de09d9856c397f82391158de57d1d1
rniman <105342620+rniman@users.noreply.github.com>
2023-04-21 22:42:03 +0900

    char*에 마지막에 널문자 추가

--- stl_task/2019182021_신민근.cpp
+++ stl_task/2019182021_신민근.cpp
@@ -23,6 +23,8 @@ public:
 	Player(Player&& other) noexcept;
 	Player& operator=(Player&& other) noexcept;
 
+	
+
 	string getName() const;
 	int getScore() const;
 	size_t getID() const;
@@ -30,11 +32,12 @@ public:
 	char* getP() const;
 
 	friend ostream& operator<<(ostream& os, const Player& player);
+	friend istream& operator>>(istream& os, Player& player);
 
 	void read(istream& is);
 	void print() const;
 
-private:
+//private:
 	string name;
 	int score;
 	size_t id;
@@ -58,127 +61,135 @@ int main()
 		return 0;
 	}
 
+
 	for (Player& player : players)
 	{
 		player.read(in);
 	}
 	
 	//1
-	cout << "1번 문제\n";
-	players[PLAYER_NUM - 1].print();
+	//cout << "1번 문제\n";
+	//players[PLAYER_NUM - 1].print();
 
-	cout << "--------------------------------------------------------------------------------" << endl << endl;
-	//2 점수:423456369
-	cout << "2번 문제\n";
+	//cout << "--------------------------------------------------------------------------------" << endl << endl;
+	////2 점수:423456369
+	//cout << "2번 문제\n";
 
-	//start_time = std::chrono::high_resolution_clock::now();
-	
-	//점수:123468028
-	long long sum = 0;
-	int max = numeric_limits<int>::min();
-	int max_index = 0;
+	////start_time = std::chrono::high_resolution_clock::now();
+	//
+	////점수:123468028
+	//long long sum = 0;
+	//int max = numeric_limits<int>::min();
+	//int max_index = 0;
 
-	sum += players[0].getScore();
-	auto max_score = max_element(players.begin(), players.end(), [&sum](const Player& a, const Player& b) {
-		sum += b.getScore();
-		return a.getScore() < b.getScore();
-		});
-	//sum = accumulate(players.begin(), players.end(), 0ll, [](long long sum, const Player& a) {
-	//	return sum + a.getScore();
+	//sum += players[0].getScore();
+	//auto max_score = max_element(players.begin(), players.end(), [&sum](const Player& a, const Player& b) {
+	//	sum += b.getScore();
+	//	return a.getScore() < b.getScore();
+	//	});
+	////sum = accumulate(players.begin(), players.end(), 0ll, [](long long sum, const Player& a) {
+	////	return sum + a.getScore();
+	////	});
+
+
+	////for (int i = 0; i < PLAYER_NUM; ++i)	
+	////{
+	////	sum += players[i].getScore();
+	////	if (max < players[i].getScore())
+	////	{
+	////		max = players[i].getScore();
+	////		max_index = i;
+	////	}
+	////}
+	//
+	//cout << "가장 큰 점수의 Player\n";
+	//max_score->print();
+	////players[max_index].print();
+	//cout <<  "모든 Player의 평균 점수:" << sum / PLAYER_NUM << endl;
+	//
+	///*end_time = std::chrono::high_resolution_clock::now();
+	//duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+	//cout << "걸린 시간: " << duration << endl;*/
+	//
+	//cout << "--------------------------------------------------------------------------------" << endl << endl;
+	////3
+	//cout << "3번 문제\n";
+
+	//sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+	//	return a.getID() < b.getID();
 	//	});
 
+	//ofstream out{ "같은아이디.txt" };
 
-	//for (int i = 0; i < PLAYER_NUM; ++i)	
+	//int allSameNum = 0;
+	//int firstIndex = 0;
+	//int sameCount = 1;
+	//size_t prevID = players[0].getID();
+	//for (int i = 1; i < PLAYER_NUM; ++i)
 	//{
-	//	sum += players[i].getScore();
-	//	if (max < players[i].getScore())
+	//	if (players[i].getID() != prevID)
 	//	{
-	//		max = players[i].getScore();
-	//		max_index = i;
+	//		if (sameCount > 1)
+	//		{
+	//			for (int j = firstIndex; j < firstIndex + sameCount; ++j)
+	//			{
+	//				++allSameNum;
+	//				out << "이름:" << players[j].getName() << ", ID:" << players[j].getID() << endl;
+	//			}
+	//		}
+	//		sameCount = 1;
+	//		firstIndex = i;
+	//		prevID = players[i].getID();
+	//		continue;
+	//	}
+	//	++sameCount;
+
+	//	if (i == PLAYER_NUM - 1)
+	//	{
+	//		if (sameCount > 1)
+	//		{
+	//			for (int j = firstIndex; j < firstIndex + sameCount; ++j)
+	//			{
+	//				++allSameNum;
+	//				out << "이름:" << players[j].getName() << ", ID:" << players[j].getID() << endl;
+	//			}
+	//		}
+	//		break;
 	//	}
 	//}
-	
-	cout << "가장 큰 점수의 Player\n";
-	max_score->print();
-	//players[max_index].print();
-	cout <<  "모든 Player의 평균 점수:" << sum / PLAYER_NUM << endl;
-	
-	/*end_time = std::chrono::high_resolution_clock::now();
-	duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
-	cout << "걸린 시간: " << duration << endl;*/
-	
-	cout << "--------------------------------------------------------------------------------" << endl << endl;
-	//3
-	cout << "3번 문제\n";
+	//cout << "ID가 같은 모든 객체의 수:" << allSameNum;
 
-	sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
-		return a.getID() < b.getID();
-		});
-
-	ofstream out{ "같은아이디.txt" };
-
-	int allSameNum = 0;
-	int firstIndex = 0;
-	int sameCount = 1;
-	size_t prevID = players[0].getID();
-	for (int i = 1; i < PLAYER_NUM; ++i)
-	{
-		if (players[i].getID() != prevID)
-		{
-			if (sameCount > 1)
-			{
-				for (int j = firstIndex; j < firstIndex + sameCount; ++j)
-				{
-					++allSameNum;
-					out << "이름:" << players[j].getName() << ", ID:" << players[j].getID() << endl;
-				}
-			}
-			sameCount = 1;
-			firstIndex = i;
-			prevID = players[i].getID();
-			continue;
-		}
-		++sameCount;
-
-		if (i == PLAYER_NUM - 1)
-		{
-			if (sameCount > 1)
-			{
-				for (int j = firstIndex; j < firstIndex + sameCount; ++j)
-				{
-					++allSameNum;
-					out << "이름:" << players[j].getName() << ", ID:" << players[j].getID() << endl;
-				}
-			}
-			break;
-		}
-	}
-	cout << "ID가 같은 모든 객체의 수:" << allSameNum;
-
-	cout << "--------------------------------------------------------------------------------" << endl << endl;
+	//cout << "--------------------------------------------------------------------------------" << endl << endl;
 	//4
 	cout << "4번 문제\n";
 
 	int moreThan10Num = 0;
 	for (Player& player : players)
 	{
+		
 		sort(player.getP(), player.getP() + player.getNum());
+		
 
-		int aNum = 0;
-		for (int i = 0; i < player.getNum(); ++i)
-		{
-			if (player.getP()[i] != 'a')
-			{
-				break;
-			}
+		cout << player.getP();
 
-			++aNum;
-		}
+		cout << endl;
+		cout << endl;
 
-		if (aNum >= 10)
-		{
-			++moreThan10Num;
-		}
+		//int aNum = 0;
+		//for (int i = 0; i < player.getNum(); ++i)
+		//{
+		//	if (player.getP()[i] != 'a')
+		//	{
+		//		break;
+		//	}
+
+		//	++aNum;
+		//}
+
+		//if (aNum >= 10)
+		//{
+		//	++moreThan10Num;
+		//}
 
 		//if (count(player.getP(), player.getP() + player.getNum(), 'a') >= 10)
 		//{
@@ -189,107 +200,107 @@ int main()
 
 	cout << "--------------------------------------------------------------------------------" <<  endl << endl;
 	//5
-	cout << "5번 문제\n";
+	//cout << "5번 문제\n";
 
-	size_t inputId;
-	cout << "ID입력(ID외의 입력시 종료):";
-	while (cin >> inputId)
-	{
-		cout << endl << "ID 정렬";
-		sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
-			return a.getID() < b.getID();
-			});
+	//size_t inputId;
+	//cout << "ID입력(ID외의 입력시 종료):";
+	//while (cin >> inputId)
+	//{
+	//	cout << endl << "ID 정렬";
+	//	sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+	//		return a.getID() < b.getID();
+	//		});
 
-		array<Player, PLAYER_NUM>::iterator itr = players.begin();
-		itr = find_if(players.begin(), players.end(), [&inputId](const Player& a) {
-			return a.getID() == inputId;
-			});
-		if (itr != players.end())
-		{
-			while (true)
-			{
-				itr->print();
+	//	array<Player, PLAYER_NUM>::iterator itr = players.begin();
+	//	itr = find_if(players.begin(), players.end(), [&inputId](const Player& a) {
+	//		return a.getID() == inputId;
+	//		});
+	//	if (itr != players.end())
+	//	{
+	//		while (true)
+	//		{
+	//			itr->print();
 
-				if (itr != players.begin())
-				{
-					(itr - 1)->print();
-				}
+	//			if (itr != players.begin())
+	//			{
+	//				(itr - 1)->print();
+	//			}
 
-				if (itr + 1 == players.end())
-				{
-					break;
-				}
-				else if (itr->getID() != (itr + 1)->getID())
-				{
-					(itr + 1)->print();
-					break;
-				}
-				else
-				{
-					(itr + 1)->print();
-					++itr;
-					cout << endl << endl;
-				}
-			}
-		}
+	//			if (itr + 1 == players.end())
+	//			{
+	//				break;
+	//			}
+	//			else if (itr->getID() != (itr + 1)->getID())
+	//			{
+	//				(itr + 1)->print();
+	//				break;
+	//			}
+	//			else
+	//			{
+	//				(itr + 1)->print();
+	//				++itr;
+	//				cout << endl << endl;
+	//			}
+	//		}
+	//	}
 
-		cout << endl << "name 정렬";
-		sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
-			return a.getName() < b.getName();
-			});
+	//	cout << endl << "name 정렬";
+	//	sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+	//		return a.getName() < b.getName();
+	//		});
 
-		itr = players.begin();
-		itr = find_if(players.begin(), players.end(), [&inputId](const Player& a) {
-			return a.getID() == inputId;
-			});
-		if (itr != players.end())
-		{
+	//	itr = players.begin();
+	//	itr = find_if(players.begin(), players.end(), [&inputId](const Player& a) {
+	//		return a.getID() == inputId;
+	//		});
+	//	if (itr != players.end())
+	//	{
 
-			itr->print();
+	//		itr->print();
 
-			if (itr != players.begin())
-			{
-				(itr - 1)->print();
-			}
+	//		if (itr != players.begin())
+	//		{
+	//			(itr - 1)->print();
+	//		}
 
-			if (itr + 1 != players.end())
-			{
-				(itr + 1)->print();
-			}
-			cout << endl << endl;
-		}
+	//		if (itr + 1 != players.end())
+	//		{
+	//			(itr + 1)->print();
+	//		}
+	//		cout << endl << endl;
+	//	}
 
-		cout << "score 정렬";
-		sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
-			return a.getScore() < b.getScore();
-			});
+	//	cout << "score 정렬";
+	//	sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+	//		return a.getScore() < b.getScore();
+	//		});
 
-		itr = players.begin();
-		itr = find_if(players.begin(), players.end(), [&inputId](const Player& a) {
-			return a.getID() == inputId;
-			});
-		if (itr != players.end())
-		{
+	//	itr = players.begin();
+	//	itr = find_if(players.begin(), players.end(), [&inputId](const Player& a) {
+	//		return a.getID() == inputId;
+	//		});
+	//	if (itr != players.end())
+	//	{
 
-			itr->print();
+	//		itr->print();
 
-			if (itr != players.begin())
-			{
-				(itr - 1)->print();
-			}
+	//		if (itr != players.begin())
+	//		{
+	//			(itr - 1)->print();
+	//		}
 
-			if (itr + 1 != players.end())
-			{
-				(itr + 1)->print();
-			}
-			cout << endl << endl;
-		}
+	//		if (itr + 1 != players.end())
+	//		{
+	//			(itr + 1)->print();
+	//		}
+	//		cout << endl << endl;
+	//	}
 
 
-		cout << "--------------------------------------------------------------------------------" << endl;
-		cout << "ID입력(ID외의 입력시 종료):";
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	}
+	//	cout << "--------------------------------------------------------------------------------" << endl;
+	//	cout << "ID입력(ID외의 입력시 종료):";
+	//	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	//}
 }
 
 Player::Player()
@@ -395,8 +406,10 @@ char* Player::getP() const
 void Player::read(istream& is)
 {
 	is.read((char*)this, sizeof Player);
-	p = new char[num];
+	p = nullptr;
+	p = new char[num + 1];
 	is.read(p, num);
+	p[num] = '\0';
 }
 
 void Player::print() const
@@ -415,4 +428,9 @@ ostream& operator<<(ostream& os, const Player& player)
 {
 	player.print();
 	return os;
+}
+
+istream& operator>>(istream& is, Player& player)
+{
+	return is.read((char*)&player, sizeof Player);
 }
